280ee3abd50943f075d6e46ebd0a2b16bf225bcc
1824-5 <1824-5@DESKTOP-7VIJGQB>
2023-09-01 18:14:06 +0900

    test

--- src/main/java/com/game/team3/filter/AuthFilter.java
+++ src/main/java/com/game/team3/filter/AuthFilter.java
@@ -1,7 +1,8 @@
 package com.game.team3.filter;
 
 import java.io.IOException;
-import java.util.Enumeration;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
@@ -9,35 +10,48 @@ import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.web.servlet.ServletComponentScan;
-import org.springframework.http.HttpHeaders;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.filter.GenericFilterBean;
 
 import com.game.team3.util.JWTToken;
+import com.game.team3.vo.UserInfoVO;
 
 import lombok.extern.slf4j.Slf4j;
 
-//@WebFilter("/*")
+@WebFilter(value = {"/","/views/*","/api/*"})
 @Slf4j
 
 public class AuthFilter extends GenericFilterBean {
 
 	@Autowired
 	private JWTToken jwtToken;
-
+	@Value("${auth.execlude.urls}")
+	private List<String> execludeUrls =new ArrayList<>();
+	{
+		execludeUrls.add("/views/login");
+		execludeUrls.add("/views/join");
+		execludeUrls.add("/api/login");
+		execludeUrls.add("/api/join");
+	}
 	@Override
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
 			throws IOException, ServletException {
 		// TODO Auto-generated method stub
-		HttpServletRequest req = (HttpServletRequest) request;
-		String token = req.getHeader(HttpHeaders.AUTHORIZATION);
-		if (token != null) {
-			log.info("userId=>{}", jwtToken.getUserIdFromToken(token));
+		if(request instanceof HttpServletRequest req && response instanceof HttpServletResponse res) {
+			String uri =req.getRequestURI();
+			if(!execludeUrls.contains(uri)) {
+				HttpSession session =req.getSession();
+				UserInfoVO user=(UserInfoVO) session.getAttribute("user");
+				if(user==null) {
+					res.sendRedirect("/views/login");
+					return;
+				}
+			}
 		}
-		log.info("token=>{}", token);
-		chain.doFilter(request, response);
 	}
 
 }
---
+++ src/main/webapp/WEB-INF/views/login.jsp
@@ -0,0 +1,14 @@
+<%@ page language="java" contentType="text/html; charset=EUC-KR"
+    pageEncoding="EUC-KR"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="EUC-KR">
+<title>Insert title here</title>
+</head>
+<body>
+  <input type="text" id="uiId" placeholder="아이디"><br>
+   <input type="password" id="uiPwd" placeholder="비밀번호"><br>
+   <button>로그인</button>
+</body>
+</html>
